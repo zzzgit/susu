c58615066e4f376c4b515dec39e63edc24a14c1c
zzzgit <zzzgoo@gmail.com>
2026-01-07 07:48:46 +0800

    2222

--- src/db.js
+++ src/db.js
@@ -10,12 +10,19 @@ const prisma = new PrismaClient({
 	adapter,
 	log: [
 		{
-			emit: 'stdout',
+			emit: 'event',
 			level: 'query',
 		},
 		'info',
 		'warn',
+		'error',
 	],
+})
+
+prisma.$on('query', (e)=> {
+	console.log('Query: ' + e.query)
+	console.log('Params: ' + e.params)
+	console.log('Duration: ' + e.duration + 'ms')
 })
 
 function normalizeCustomer(row){
